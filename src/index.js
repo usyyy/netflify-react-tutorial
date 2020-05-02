f83eab73c8b996ba2b7b87df6d5bec1778c6d337
Usmaan Ali <usmaan.ali@radiuspaymentsolutions.com>
2020-05-02 22:50:53 +0100

    WIP

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ import App from './App';
 
 const title = 'My Minimal React Webpack Babel Setup';
 
+const API_URL = 'https://quiet-gorge-59752.herokuapp.com';
+
+fetch(`${ API_URL }/ayats`)
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.log(error));
+
 ReactDOM.render(
   <App title={title} />,
   document.getElementById('app')
